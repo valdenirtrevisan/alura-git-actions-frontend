28d3092d92286d98411c84461c9d22cca5ae7b4d
Valdenir Trevisan Junior <valdenirtrevi@gmail.com>
2024-02-13 07:20:23 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function HomeScreen() {
   return (
     <div>
       <h1>Página Inicial</h1>
+      <Link href="/sobre"> Ir para Sobre</Link>
     </div>
   );
 }
