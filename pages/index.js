c277e93af92a3bb4820ed2cf34e1a4926753b885
Valdenir Trevisan Junior <valdenirtrevi@gmail.com>
2024-02-13 07:32:04 -0300

    up

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function HomeScreen() {
   return (
     <div>
-      <h1>Página Inicial 2</h1>
+      <h1>Página Inicial 3</h1>
       <Link href="/sobre"> Ir para Sobre</Link>
     </div>
   );
